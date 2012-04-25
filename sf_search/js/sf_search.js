394eb8f5ce1db34412cd08fa2bab20c5326f669d
Phillip Cave <phillip.cave@jacksonriver.com>
2012-04-25 16:13:31 -0400

    In-progress additions to sf_search module.

--- sf_search/js/sf_search.js
+++ sf_search/js/sf_search.js
@@ -4,44 +4,43 @@ sfSearch.contacts = []; // initialize empty contacts array
 
 Drupal.behaviors.SfSearchSetup = function(context) {
 
+  $('#sf-search-search-form').submit(function() {
+    var query = $('#edit-sf-search-query').val();
+    var nid = $('#edit-nid').val();
+    var fid = $('#edit-fid').val();
+    
+    // hide any current search results
+    $('#search-results').slideUp('fast');
+    
+    // search contacts
+    contactSearch(query, nid, fid);
+ 
+    return false;
+  });
+
   $('#sf-search-lookup-form').submit(function() {
     var donor_id = $('#edit-sf-search-donor-id').val();
     var nid = $('#edit-nid').val();
     var fid = $('#edit-fid').val();
-
-    var url = '/js/salesforce-search/lookup/' + nid + '/' + fid + '/' + donor_id;
-
-    // make an ajax request to the endpoint
-    $.getJSON(url, function(data) {
-      // check how much data we got back
-      if (data.length == 1) {
-        // single contact, go ahead and populate form
-        populateContactInfo(data[0]);
-      }
-      else if (data.length > 1) {
-        // multiple contacts, return some formatted html for selecting a contact
-        $('#search-results').html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="sf-contacts"></table>');
-                
-        // save data for later
-        sfSearch.contacts = data;
-        
-        // get a data tables friendly version of the column data
-        columns = getColumnDefinitions();
-        
-        // create a data table on the fly filled with the returned contacts
-        contactTable = $('#sf-contacts').dataTable( {
-          "aaData": sfSearch.contacts,
-          "aoColumns": columns
-        });
-      }
-      else {
-        // TODO: Add something to indicate no contacts where found
-      }
-    });
-
-    return false;
     
+    // hide any current search results
+    $('#search-results').slideUp('fast');
+    
+    // lookup the contact
+    contactLookup(donor_id, nid, fid);
+ 
+    return false;
   });
+  
+  /* throbbers */
+  $("#sf-search-lookup-loading").bind("ajaxStart", function(){
+    $(this).show();
+  });
+  
+  $("#sf-search-lookup-loading").bind("ajaxStop", function(){
+    $(this).hide();
+  });
+  
 };
 
 /**
@@ -50,6 +49,7 @@ Drupal.behaviors.SfSearchSetup = function(context) {
 $("#sf-contacts tbody tr a.donor-id").live('click',function(){
     var position = $('#sf-contacts').dataTable().fnGetPosition(this.parentNode);
     var contact = $('#sf-contacts').dataTable().fnGetData(position[0]);
+    $('#search-results').slideUp('fast');
     populateContactInfo(contact);
 });
 
@@ -78,13 +78,75 @@ function getColumnDefinitions() {
 }
 
 /**
+ * Displays the results of a search query.
+ */
+function displayResults(data) {
+  if (data.length == 1) {
+    // single contact, go ahead and populate form
+    populateContactInfo(data[0]);
+  }
+  else if (data.length > 1) {
+    // multiple contacts, return some formatted html for selecting a contact
+    $('#search-results').html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="sf-contacts"></table>');
+            
+    // save data for later
+    sfSearch.contacts = data;
+    
+    // get a data tables friendly version of the column data
+    columns = getColumnDefinitions();
+    
+    // create a data table on the fly filled with the returned contacts
+    contactTable = $('#sf-contacts').dataTable( {
+      "aaData": sfSearch.contacts,
+      "aoColumns": columns
+    });
+    
+    // display the results
+    $('#search-results').slideDown('fast');
+  }
+  else {
+    $('#search-results').html('<em>No contacts found.</em>').show().fadeOut(3000);
+  }
+}
+
+/**
+ * Performs a contact lookup by donor id.
+ */
+function contactLookup(donor_id, nid, fid) {
+  var url = '/js/salesforce-search/lookup/' + nid + '/' + donor_id;
+  $.getJSON(url, function(data) {
+    displayResults(data);
+  });
+}
+
+/**
+ * Performs a full-text contact search.
+ */
+function contactSearch(query, nid, fid) {
+  var url = '/js/salesforce-search/find/' + nid + '/' + query;
+  $.getJSON(url, function(data) {
+    displayResults(data);
+  });
+}
+
+/**
  * Takes a contact object and uses a client-side fieldmap to populate a form.
  */
 function populateContactInfo(contact) {
   var fieldMap = Drupal.settings.sf_search.field_map;
   for (var field in fieldMap) {
     var inputId = '#' + fieldMap[field];
-    $(inputId).val(contact[field]);
-    // TODO: Add some effect so that user can see which fields populated
+    
+    // there has to be a better solution for these state/country conversions
+    if (field == 'MailingState') {
+      contact[field] = Drupal.settings.sf_search.zones[contact[field]];
+    }
+    
+    if (field == 'MailingCountry') {
+      contact[field] = Drupal.settings.sf_search.countries[contact[field]];
+    }
+    
+    
+    $(inputId).val(contact[field]).effect('highlight', {}, 2000);
   }
 }
